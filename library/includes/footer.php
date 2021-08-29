eddc7741f3288017b07673df92a72ca193c1d69c
itxutkarsh <85636900+itxutkarsh@users.noreply.github.com>
2021-08-29 10:25:07 +0530

    Add files via upload
    if you want to learn this project you need XAMPP to create an MY Sql server offline to run this project like online.

---
+++ library/includes/footer.php
@@ -0,0 +1,10 @@
+   <section class="footer-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                   &copy; 2021 Online Library Management System |<a href="https://www.linkedin.com/in/utkarsh-baranwal-73977914a/" target="_blank"  > Designed by : Utkarsh Baranwal</a> 
+                </div>
+
+            </div>
+        </div>
+    </section>
